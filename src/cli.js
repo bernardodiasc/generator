63fa2c44cfd83222b7b52bcec96f178cc2e65455
Bernardo Dias <bernardodiasdacruz@gmail.com>
2018-02-14 16:23:37 -0200

    Initial commit, basic structure

---
+++ src/cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env babel-node
+
+import lib from '.'
+
+lib()
---
+++ src/index.js
@@ -0,0 +1,9 @@
+function generator () {
+  try {
+    console.log('Generating stuff...')
+  } catch (e) {
+    console.log('Error: ', e)
+  }
+}
+
+export default generator
